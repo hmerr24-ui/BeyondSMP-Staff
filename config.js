42546b6633e62df430cfbf32b4a13b34aa8dc95e
Vyzor <hmerr24@gmail.com>
2026-08-10 01:27:14 +0100

    v2 - patch

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // This file may be published with the site.
 // NEVER put a Supabase Secret/service_role key here.
 window.BEYOND_SUPABASE = {
-  url: "PASTE_SUPABASE_URL_HERE",
-  publishableKey: "PASTE_SUPABASE_PUBLISHABLE_KEY_HERE"
+  url: "https://nycvoburyxlgjuuutybw.supabase.co",
+  publishableKey: "sb_publishable_kcKaghUNN_Im3J3rbNbMKg_S0ESCLcc"
 };
